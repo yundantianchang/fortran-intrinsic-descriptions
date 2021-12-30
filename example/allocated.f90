d3ef7880ed475270e73da1b656b627b35fef14f9
John S. Urban <urbanjost@comcast.net>
2021-12-29 22:13:54 -0500

    clean_main

--- example/allocated.f90
+++
@@ -1,9 +0,0 @@
-    program demo_allocated
-    use,intrinsic :: iso_fortran_env, only : dp=>real64,sp=>real32
-    implicit none
-    integer :: i = 4
-    real(kind=sp), allocatable :: x(:)
-
-       if (allocated(x) .eqv. .false.) allocate(x(i))
-
-    end program demo_allocated
--- example/command_argument_count.f90
+++
@@ -1,6 +0,0 @@
-    program demo_command_argument_count
-    implicit none
-    integer :: count
-       count = command_argument_count()
-       print *, count
-    end program demo_command_argument_count
