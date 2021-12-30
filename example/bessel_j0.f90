6c61ea9cb25486ff1d97830006c194db047d220e
John S. Urban <urbanjost@comcast.net>
2021-12-29 22:17:47 -0500

    clean

--- example/bessel_j0.f90
+++
@@ -1,8 +0,0 @@
-    program demo_besj0
-    use, intrinsic :: iso_fortran_env, only : real_kinds, &
-    & real32, real64, real128
-       implicit none
-       real(kind=real64) :: x = 0.0_real64
-       x = bessel_j0(x)
-       write(*,*)x
-    end program demo_besj0
--- example/extends_type_of.f90
+++
@@ -1,2 +0,0 @@
- !<ERROR> *fman* standard demo code format not found for extends_type_of
-
